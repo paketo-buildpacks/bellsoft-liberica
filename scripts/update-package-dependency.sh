2cfbc90e183f01a32df96d56d8c1588d05e9af1c
Ben Hale <bhale@pivotal.io>
2020-04-07 11:18:14 -0700

    Remove repository-resource dependency
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- scripts/update-package-dependency.sh
+++ scripts/update-package-dependency.sh
@@ -1,3 +1,23 @@
+sha256() {
+  if [[ "${DEPENDENCY}" == "jvmkill" ]]; then
+    shasum -a 256 "${ROOT}"/dependency/jvmkill-*.so | cut -f 1 -d ' '
+  elif [[ "${DEPENDENCY}" == "memory-calculator" ]]; then
+    shasum -a 256 "${ROOT}"/dependency/memory-calculator-*.tgz | cut -f 1 -d ' '
+  else
+    cat "${ROOT}"/dependency/sha256
+  fi
+}
+
+uri() {
+  if [[ "${DEPENDENCY}" == "jvmkill" ]]; then
+    echo "https://github.com/cloudfoundry/jvmkill/releases/download/v$(cat "${ROOT}"/dependency/version)/$(basename "${ROOT}"/dependency/jvmkill-*.so)"
+  elif [[ "${DEPENDENCY}" == "memory-calculator" ]]; then
+    echo "https://github.com/cloudfoundry/java-buildpack-memory-calculator/releases/download/v$(cat "${ROOT}"/dependency/version)/$(basename "${ROOT}"/dependency/memory-calculator-*.tgz)"
+  else
+    cat "${ROOT}"/dependency/uri
+  fi
+}
+
 version() {
   local PATTERN="([0-9]+)\.([0-9]+)\.([0-9]+)(.*)"
 
