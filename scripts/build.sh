ebd2c67136cfe80cee0e30477acb16a8d49b553d
Ben Hale <bhale@vmware.com>
2020-05-22 06:29:24 -0700

    Remove openssl-security-provider dependency
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,7 @@ fi
 GOOS="linux" go build -ldflags='-s -w' -o bin/class-counter github.com/paketo-buildpacks/libjvm/cmd/class-counter
 GOOS="linux" go build -ldflags='-s -w' -o bin/link-local-dns github.com/paketo-buildpacks/libjvm/cmd/link-local-dns
 GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/paketo-buildpacks/bellsoft-liberica/cmd/main
+GOOS="linux" go build -ldflags='-s -w' -o bin/openssl-certificate-loader github.com/paketo-buildpacks/libjvm/cmd/openssl-certificate-loader
 GOOS="linux" go build -ldflags='-s -w' -o bin/security-providers-configurer github.com/paketo-buildpacks/libjvm/cmd/security-providers-configurer
 ln -fs main bin/build
 ln -fs main bin/detect
