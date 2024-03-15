20153184825ef2b5861fb7def69746b9665eada7
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-03-15 21:36:58 +0000

    Add dualarch configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,20 @@
 
 set -euo pipefail
 
-GOOS="linux" go build -ldflags='-s -w' -o bin/helper github.com/paketo-buildpacks/libjvm/cmd/helper
-GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/paketo-buildpacks/bellsoft-liberica/v9/cmd/main
+GOOS="linux" go build -ldflags='-s -w' -o linux/amd64/bin/main github.com/paketo-buildpacks/bellsoft-liberica/v9/cmd/main
+GOOS="linux" go build -ldflags='-s -w' -o linux/amd64/bin/helper github.com/paketo-buildpacks/libjvm/cmd/helper
+GOOS="linux" GOARCH="arm64" go build -ldflags='-s -w' -o linux/arm64/bin/main github.com/paketo-buildpacks/bellsoft-liberica/v9/cmd/main
+GOOS="linux" GOARCH="arm64" go build -ldflags='-s -w' -o linux/arm64/bin/helper github.com/paketo-buildpacks/libjvm/cmd/helper
 
 if [ "${STRIP:-false}" != "false" ]; then
-  strip bin/helper bin/main
+  strip linux/amd64/bin/main linux/arm64/bin/main linux/amd64/bin/helper linux/arm64/bin/helper
 fi
 
 if [ "${COMPRESS:-none}" != "none" ]; then
-  $COMPRESS bin/helper bin/main
+  $COMPRESS linux/amd64/bin/main linux/arm64/bin/main linux/amd64/bin/helper linux/arm64/bin/helper
 fi
 
-ln -fs main bin/build
-ln -fs main bin/detect
+ln -fs main linux/amd64/bin/build
+ln -fs main linux/arm64/bin/build
+ln -fs main linux/amd64/bin/detect
+ln -fs main linux/arm64/bin/detect
